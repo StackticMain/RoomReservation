e9991696df4eebf6ac87fa69115a255170c66c3a
StackticMain <shaheer@tribecatreehouse.com>
2015-03-17 01:09:26 -0400

    Initial Commit
    Basic commit to get started on the work for this project

---
+++ RoomReservationContracts/RoomReservation.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+using System.Runtime.Serialization;
+
+namespace RoomReservationContracts
+{
+    public class RoomReservation : INotifyPropertyChanged
+    {
+        private int id;
+
+        public int Id
+        {
+            get { return id;  }
+            set { SetProperty(ref id, value); }
+        }
+
+        protected virtual void SetProperty<T>(ref T item, T value, [CallerMemberName] string propertyName = null)
+        {
+            if(!EqualityComparer<T>.Default.Equals(item, value))
+            {
+                item = value;
+                OnNotifyPropertyChanged(propertyName);
+            }
+        }
+
+        protected virtual void OnNotifyPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler eventHandler = PropertyChanged;
+
+        }
+
+    }
+}
